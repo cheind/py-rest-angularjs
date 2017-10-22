be794d27e2828a63096391f478e960bcc2753fca
Christoph Heindl <christoph.heindl@gmail.com>
2017-10-22 09:05:41 +0200

    little refactoring and code docs

--- static/controller.js
+++ static/controller.js
@@ -1,15 +1,21 @@
 'use strict';
 
+/*
+A controller for 'main.html' page that handles communication with the 
+REST API exposed by the Python backend.
+*/
 angular.module('demoapp')
 .controller('RestAPICtrl',  ['$scope', '$http', function($scope, $http) {
 
-    $scope.result = null;
+    $scope.message = null;
+    $scope.name = 'Anonymous'
 
-    $scope.invokeAPI = function(){
-        $http.get('/api').
-            then(function(response) {
-                $scope.result = response.data;
-            });
+    $scope.greet = function(){
+        $http
+        .get('/api/greet/' + $scope.name) // invoke backend API
+        .then(function(response) {
+            $scope.message = response.data['message'];
+        });
     }
 
 }]);
--- static/routes.js
+++ static/routes.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/* 
+All AngularJS local routes are defined here. 
+
+Note, adding a route here requires the same route to be added 
+to the backend (flask / aiohttp), Otherwise calling 'localhost/xx' 
+will be handled by the backend and not AngularJS.
+*/
 angular
 .module('demoapp')
 .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {    
